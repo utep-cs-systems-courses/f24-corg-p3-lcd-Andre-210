a51b079168d593aa1310a978e4b10dff37cbe543
Andre Melendez <andre.melendez5835@gmail.com>
2024-11-13 12:20:52 -0700

    Create buzzer.h

---
+++ project/buzzer.h
@@ -0,0 +1,12 @@
+#ifndef buzzer_included
+#define buzzer_included
+
+// function prototypes
+void buzzer_init();                      // initializes the buzzer hardware
+void buzzer_set_period(short cycles);    // cycles
+//void fur_Elise();                        // a song
+//void happy_Bday();                       // a song
+//void Mario();                            // a song
+//void runaway();                          // a song
+
+#endif // included
