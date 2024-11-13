969c09891b6a64484a33074bf436babfbeeff466
Andre Melendez <andre.melendez5835@gmail.com>
2024-11-13 14:46:30 -0700

    Update main.c

--- project/main.c
+++ project/main.c
@@ -4,8 +4,19 @@
 #include "switches.h"
 #include "buzzer.h"
 
+#include "lcdutils.h"
+#include "lcddraw.h"
+
 int main(void) {
+  
+  // ADDED
+  P1DIR |= LED;		/**< Green led on when CPU on */
+  P1OUT |= LED;
+  //
   configureClocks();
+  // ADDED
+  lcd_init();
+  //
   switch_init();              // initialize switches
   led_init();                 // initialize led
   buzzer_init();            // initialize buzzer
@@ -13,5 +24,18 @@ int main(void) {
   green_led_on();             // turn on the green led
   enableWDTInterrupts();      // enable WatchDog interrupts
 
-  or_sr(0x18); // CPU off, GIE on   
+  // ADDED
+  or_sr(0x8);	              /**< GIE (enable interrupts) */
+  
+  clearScreen(BG_COLOR);
+  while (1) {			/* forever */
+    if (redrawScreen) {
+      redrawScreen = 0;
+      update_shape();
+    }
+    P1OUT &= ~LED;	/* led off */
+    or_sr(0x10);	/**< CPU OFF */
+    P1OUT |= LED;	/* led on */
+  }
+  //
 }
