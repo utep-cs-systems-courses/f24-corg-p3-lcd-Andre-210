36597946339446c723d4a46ce3c042c875b39ffa
Andre Melendez <andre.melendez5835@gmail.com>
2024-11-21 20:48:53 -0800

    Update song_images.c

--- project/song_images.c
+++ project/song_images.c
@@ -3,15 +3,41 @@
 
 void drawDefault() {
   clearScreen(COLOR_BLUE);
+  // find the height and width of the screen
   u_char width = screenWidth, height = screenHeight;
-  int col = 0, row = 0; 
-  int centerCol = width/2, centerRow = height/4;
-  for (row = 0; row < 20; row++) {
-    for (col = -row; col <= row; col+=2) {
+  // find the location of the top triangle
+  int centerCol = width / 2, centerRow = height / 4;
+  // set the length it each triangle
+  int sideLength = 20; 
+
+  // draw the top triangle
+  for (int row = 0; row < sideLength; row++) {
+    for (int col = -row; col <= row; col++) {
       drawPixel(centerCol + col, centerRow + row, COLOR_PINK);
     }
   }
+
+  // draw the bottom-left triangle
+  // find the location of the triangle
+  int leftCenterCol = centerCol - sideLength;
+  int leftCenterRow = centerRow + sideLength;
+  for (int row = 0; row < sideLength; row++) {
+    for (int col = -row; col <= row; col++) {
+      drawPixel(leftCenterCol + col, leftCenterRow + row, COLOR_PINK);
+    }
+  }
+
+  // draw the bottom-right triangle
+  // find the location of the triangle
+  int rightCenterCol = centerCol + sideLength;
+  int rightCenterRow = centerRow + sideLength;
+  for (int row = 0; row < sideLength; row++) {
+    for (int col = -row; col <= row; col++) {
+      drawPixel(rightCenterCol + col, rightCenterRow + row, COLOR_PINK);
+    }
+  }
 }
+
 void drawZeldasLullaby() {
   clearScreen(COLOR_RED);
   drawString5x7(30, 30, "Zelda's Lullaby", COLOR_WHITE, COLOR_RED);
