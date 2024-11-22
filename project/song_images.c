0f5b1f81b81352c3e997e98c926e09cf4eef650d
Andre Melendez <andre.melendez5835@gmail.com>
2024-11-22 10:01:59 -0800

    Update song_images.c

--- project/song_images.c
+++ project/song_images.c
@@ -78,43 +78,54 @@ void drawDefault(u_int colorBGR, const char *name) {
   //}
 //}
 
-//void drawOOT() {
-  //clearScreen(COLOR_ORANGE);
-  //drawString5x7(10, 15, "The Legend of Zelda", COLOR_YELLOW, COLOR_BLUE);
-  // find the height and width of the screen
-  //u_char width = screenWidth, height = screenHeight;
-  // find the location of the top triangle
-  //int centerCol = width / 2, centerRow = height / 4;
-  // set the length it each triangle
-  //int sideLength = 20; 
+void drawHyruleShield(u_int x, u_int y, u_int size) {
+  u_int shieldColor = COLOR_BLUE;   // Base color of the shield
+  u_int trimColor = COLOR_SILVER;  // Color for shield trim
+  u_int emblemColor = COLOR_RED;   // Color for the Hyrule crest
+  u_int triforceColor = COLOR_GOLD; // Color for the Triforce
 
-  // draw the top triangle
-  //for (int row = 0; row < sideLength; row++) {
-    //for (int col = -row; col <= row; col++) {
-      //drawPixel(centerCol + col, centerRow + row, COLOR_YELLOW);
-    //}
-  //}
+  // Outer shape (shield trim)
+  for (int row = 0; row < size; row++) {
+    for (int col = -row; col <= row; col++) {
+      drawPixel(x + col, y + row, trimColor);
+    }
+  }
 
-  // draw the bottom-left triangle
-  // find the location of the triangle
-  //int leftCenterCol = centerCol - sideLength;
-  //int leftCenterRow = centerRow + sideLength;
-  //for (int row = 0; row < sideLength; row++) {
-    //for (int col = -row; col <= row; col++) {
-      //drawPixel(leftCenterCol + col, leftCenterRow + row, COLOR_YELLOW);
-    //}
-  //}
+  // Inner shape (blue part of the shield)
+  for (int row = 1; row < size - 2; row++) {
+    for (int col = -(row - 1); col <= (row - 1); col++) {
+      drawPixel(x + col, y + row, shieldColor);
+    }
+  }
 
-  // draw the bottom-right triangle
-  // find the location of the triangle
-  //int rightCenterCol = centerCol + sideLength;
-  //int rightCenterRow = centerRow + sideLength;
-  //for (int row = 0; row < sideLength; row++) {
-    //for (int col = -row; col <= row; col++) {
-      //drawPixel(rightCenterCol + col, rightCenterRow + row, COLOR_YELLOW);
-    //}
-  //}
-//}
+  // Triforce emblem
+  int triforceBaseRow = y + size / 3;
+  int triforceBaseCol = x;
+  int triforceHeight = size / 6;
+
+  for (int row = 0; row < triforceHeight; row++) {
+    for (int col = -row; col <= row; col++) {
+      drawPixel(triforceBaseCol + col, triforceBaseRow + row, triforceColor);
+    }
+  }
+
+  // Red Hylian crest
+  int crestRowStart = triforceBaseRow + triforceHeight + 2;
+  int crestWidth = size / 4;
+
+  for (int row = 0; row < triforceHeight; row++) {
+    for (int col = -crestWidth + row; col <= crestWidth - row; col++) {
+      drawPixel(x + col, crestRowStart + row, emblemColor);
+    }
+  }
+
+  // Bottom shield points (adding to the triangular base)
+  for (int row = size - 3; row < size; row++) {
+    for (int col = -2; col <= 2; col++) {
+      drawPixel(x + col, y + row, trimColor);
+    }
+  }
+}
 
 //void drawSOH() {
   //clearScreen(COLOR_BROWN);
