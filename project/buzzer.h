deb1d268b39a378a70b498ba0b9f67f5f308f5a5
Andre Melendez <andre.melendez5835@gmail.com>
2024-11-22 09:08:24 -0800

    Update buzzer.h

--- project/buzzer.h
+++ project/buzzer.h
@@ -6,7 +6,7 @@ void buzzer_init();                      // initializes the buzzer hardware
 void buzzer_set_period(short cycles);    // cycles
 void zeldas_lullaby();                   // Zelda's Lullaby
 void oot();                              // Ocarina of Time Main Theme
-//void soh();                              // Song of Healing
+void soh();                              // Song of Healing
 void tp();                               // Twilight Princess Theme
 
 #endif // included
