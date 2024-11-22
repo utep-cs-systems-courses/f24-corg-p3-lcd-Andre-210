0b6272c3c32dd7d27f185a07931c4da310ae4474
Andre Melendez <andre.melendez5835@gmail.com>
2024-11-21 18:21:51 -0800

    Update buzzer.c

--- project/buzzer.c
+++ project/buzzer.c
@@ -5,8 +5,7 @@
 #include "statemachines.h"
 #include "led.h"
 
-void buzzer_init()
-{
+void buzzer_init() {
     timerAUpmode();		/* used to drive speaker */
     P2SEL2 &= ~(BIT6 | BIT7);
     P2SEL &= ~BIT7; 
@@ -14,76 +13,75 @@ void buzzer_init()
     P2DIR = BIT6;		/* enable output to speaker (P2.6) */
 }
 
-void buzzer_set_period(short cycles)
-{
+void buzzer_set_period(short cycles) {
   CCR0 = cycles; 
   CCR1 = cycles >> 1;	
 }
 
-void playTune(int *notes, int*tempo, int noteAmt)
-{
-  for(int i = 0; i < noteAmt; i++)
-  {
-    if((i % 2) == 0 ){
+void playTune(int *notes, int*tempo, int noteAmt) {
+  for(int i = 0; i < noteAmt; i++) {
+    if((i % 2) == 0 ) {
       red_led_on();
-    }else{
+    }
+    else {
       green_led_on();
     }
-
     buzzer_set_period(1000000 / notes[i]);
     int dur = tempo[i];
-    while(dur--)
-    {
-    __delay_cycles(10000);
-
+    while(dur--) {
+	__delay_cycles(10000);
     }
     leds_off();
   }
-  buzzer_set_period(bk);             // turn off buzzer
-  // P2SEL &= ~BIT6;                    // disable buzzer pin
-  //P2DIR &= ~BIT6;                    // enable pin to input mode
-  //__delay_cycles(100000);            // grace period
+  // reset the buzzer state
+  buzzer_set_period(0);                 // turn off buzzer CHANGED FROM bk TO 0
+  leds_off();                           // endsure leds are off
 }
 
-void zeldas_lullaby() // Zelda's Lullaby
-{
-
+void zeldas_lullaby() { // Zelda's Lullaby
+  // notes for song
   int notes[] = {C3, G3, E4, C3, A3, Fsharp4, C3, G3, E4, C3, A3, Fsharp4, C3, B4, G3};
+  // tempo for each note
   int tempo[] = {500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 750, 250, 500};
-
+  // play the song
   int noteAmt = sizeof(notes)/sizeof(notes[0]);
   playTune(notes,tempo,noteAmt);
-  buzzer_set_period(bk);
+  // reset the buzzer state
+  buzzer_set_period(0); // CHANGED FROM bk TO 0
 }
 
-void oot() // Ocarina of Time Theme
-{
+void oot() { // Ocarina of Time Theme
+  // notes for song
   int notes[] = {F2, F3, A3, D4, E4, C2, G3, B3, D4, E4, F2, C3, F3, A3, D4, E4, C2, G2, E3, B3, D4, E4, D5, D6, F2, C3, F3, A3, D4, Csharp6, C6, E4, B5, C2, G2, E3, B3, A5, D4, G5, E4};
+  // tempo for each note
   int tempo[] ={1276, 300, 300, 300, 1276, 1276, 300, 300, 300, 1276, 1276, 600, 500, 500, 500, 1276, 600, 500, 500, 500, 400, 500, 500, 1276, 600, 300, 300, 300, 300, 600, 1276, 300, 300, 300, 300, 300, 300, 1276, 600, 300, 300, 500, 300, 500, 1276};
+  // play the song
   int noteAmt = sizeof(notes)/sizeof(notes[0]);
   playTune(notes,tempo,noteAmt);
-  buzzer_set_period(bk);
+  // reset the buzzer state
+  buzzer_set_period(bk); // CHANGED FROM bk TO 0
 }
 
-void soh() // Song of Healing
-{
+void soh() { // Song of Healing
+  // notes for song
   int notes[] = {F3, B4, C4, A4, C4, F4, C4, F3, B4, C4, A4, C4, F4, C4, F3, B4, C4, A4, E4, D4, C4, E4, E3, B3, B3, B3, F3, B4, C4, A4, C4, F4, C4, F3, B4, C4, A4, C4, F4, C4, F3, B4, C4};
+  // tempo for each note
   int tempo[] = {500, 500, 400, 400, 400, 500, 400, 500, 500, 400, 400, 400, 500, 400, 500, 500, 400, 400, 400, 500, 500, 500, 600, 600, 600, 600, 600, 500, 500, 400, 400, 400, 500, 400, 500, 500, 400, 400, 400, 500, 400, 500, 500, 400}; 
+  // play the song
   int noteAmt = sizeof(notes)/sizeof(notes[0]);
   playTune(notes,tempo,noteAmt);
-  buzzer_set_period(bk);
+  // reset the buzzer state
+  buzzer_set_period(bk); // CHANGED FROM bk TO 0
 }
 
-void tp() // Twilight Princess Theme
-{
+void tp() { // Twilight Princess Theme
+  // notes for the song
   int notes[] = {D3, E3, F3, A3, C4, E4, D3, E3, F3, A3, C4, E4, D3, E3, F3, A3, C4, E4, G4, E4, C4, A3, F3, E3, A4, F5, E5, C5, B4, A4, G4, B4, D6, A6, E6, D6};
-  int tempo[] = {        400, 400, 400, 400, 400, 400, 
-			 400, 400, 400, 400, 400, 400,
-			 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300,
-			 500, 400, 400, 400, 400, 400, 400, 400,
-			 300, 300, 300, 300};
-
+  // tempo for each note
+  int tempo[] = {400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 500, 400, 400, 400, 400, 400, 400, 400, 300, 300, 300, 300};
+  // play the song
   int noteAmt = sizeof(notes)/sizeof(notes[0]);
   playTune(notes,tempo,noteAmt);
-  buzzer_set_period(bk);
+  // reset the buzzer state
+  buzzer_set_period(bk); // CHANGED FROM bk TO 0
 }
