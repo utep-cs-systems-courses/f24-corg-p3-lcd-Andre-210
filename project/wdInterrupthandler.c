3b6a83cfa1e8e433326a58e9540dc84993532c51
Andre Melendez <andre.melendez5835@gmail.com>
2024-11-21 18:28:21 -0800

    Update wdInterrupthandler.c

--- project/wdInterrupthandler.c
+++ project/wdInterrupthandler.c
@@ -2,8 +2,7 @@
 #include "statemachines.h"
 
 
-void
-__interrupt_vec(WDT_VECTOR) WDT() { // WatchDog Timer
+void__interrupt_vec(WDT_VECTOR) WDT() { // WatchDog Timer
   // counter to keep track of how many times interrupts have been triggered
   static char blink_count = 0;
   // increment the counter by 1, if the counter reaches 125, reset it
