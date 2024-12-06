e295fc2ef36c1e13c7ebbe77ba6c1e53fbbfdb16
Andre Melendez <andre.melendez5835@gmail.com>
2024-12-05 16:33:22 -0800

    Update statemachines.c

--- project/statemachines.c
+++ project/statemachines.c
@@ -3,56 +3,43 @@
 #include "switches.h"
 #include "statemachines.h"
 #include "buzzer.h"
-
 #include "lcdutils.h"
 #include "lcddraw.h"
 #include "song_images.h"
 
 void next_state(int state) {
   leds_off();
-  
   switch(state) {
   case 1:
     led_flash(3);
     leds_off();
     red_led_on();
-    //drawZeldasLullaby();
     drawHarp(screenWidth / 2, screenHeight / 3, 60, COLOR_BROWN, "Zelda's Lullaby");
     zeldas_lullaby();
-    //oot(); // added
-    //tp();
     leds_off();
     break;
   case 2:
     led_flash(3);
     leds_off();
     red_led_on();
-    //drawOOT();
-    //drawDefault(COLOR_TURQUOISE, "Ocarina of Time");
     drawHyruleShieldWithSword(64, 50, 40, COLOR_FOREST_GREEN, "Ocarina of Time"); // Draw the Hyrule Shield at center
     oot();
-    //tp();
     leds_off();
     break;
   case 3:
     led_flash(3);
     leds_off();
     red_led_on();
-    //drawSOH();
     drawHarp(screenWidth / 2, screenHeight / 3, 60, COLOR_PURPLE, "Majora's Mask");
     soh();
-    //oot(); // added
-    //tp();
     leds_off();
     break;
   case 4:
     led_flash(3);
     leds_off();
     red_led_on();
-    //drawTP();
     drawHyruleShieldWithSword(64, 50, 40, COLOR_DARK_GREEN, "Twilight Princess"); // Draw the Hyrule Shield at center
     tp();
-    //oot(); // added
     leds_off();
     break;
   default:
