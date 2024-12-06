91f49cf3a4af81b7cb18d33bca3df00e0c245dad
Andre Melendez <andre.melendez5835@gmail.com>
2024-12-05 16:27:49 -0800

    Update main.c

--- project/main.c
+++ project/main.c
@@ -3,31 +3,17 @@
 #include "led.h"
 #include "switches.h"
 #include "buzzer.h"
-#include "lcdutils.h" // ADDED
-#include "lcddraw.h" // ADDED
-
+#include "lcdutils.h"
+#include "lcddraw.h"
 #include "song_images.h"
 
 int main(void) {
   configureClocks();
-  lcd_init(); // ADDED
-  //drawDefault();
-  //u_char width = screenWidth, height = screenHeight; // ADDED
-  //clearScreen(COLOR_BLUE); // ADDED
-  //int col = 0, row = 0; // ADDED
-  //int centerCol = width/2, centerRow = height/2; // ADDED
-  //for (row = 0; row < 20; row++) { // ADDED
-    //for (col = -row; col <= row; col+=2) { // ADDED
-      //drawPixel(centerCol + col, centerRow + row, COLOR_PINK); // ADDED
-      //}
-    //}
-  
+  lcd_init();                 // initialize lcd screen
   switch_init();              // initialize switches
   led_init();                 // initialize led
-  buzzer_init();            // initialize buzzer
-  
+  buzzer_init();              // initialize buzzer
   green_led_on();             // turn on the green led
   enableWDTInterrupts();      // enable WatchDog interrupts
-
-  or_sr(0x18); // CPU off, GIE on
+  or_sr(0x18);                // CPU off, GIE on
 }
